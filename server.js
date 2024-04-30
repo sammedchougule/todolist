165fb436024417b62e13d5b010e0fde43ae9ae97
sammedchougule <smdchougule24@gmail.com>
2024-04-30 21:25:49 +0530

    completed todo list in express

--- server.js
+++ server.js
@@ -4,20 +4,32 @@ const express = require("express")
 // Create a new express application object
 const app = express()
 
-//middleware
-app.use("/static", express.static("static")
-)
+// Array for Todos
+const todos = []
 
-//Routes
+// Middleware
+app.use("/static", express.static("static"))
+app.use(express.urlencoded({extended: true}))
+
+// Routes
 app.get("/test", (req, res) => {
     res.send("Hello")
 })
 
 app.get("/", (req, res) => {
     res.render("index.ejs", {
-        name: "Sammed Chougule"
+        name: "Sammed Chougule",
+        todos: todos
     })
 })
+
+app.post("/", (req, res) => {
+    // push new todo into array
+    todos.push(req.body)
+    // redirect back to main page (refresh page)
+    res.redirect("/")
+})
+
 
 // run your application, so it listens on port 4444
 app.listen(4444, () => {
